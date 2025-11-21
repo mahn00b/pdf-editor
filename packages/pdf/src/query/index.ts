7a3de3dfa87bfa0f36ef6529b7b409fb4deefdc2
Mahmoud Yousif <myousif123@gmail.com>
2025-11-21 10:35:19 -0500

    feat(pdf): Exporting utils as individuals modules

---
+++ packages/pdf/src/query/index.ts
@@ -0,0 +1,3 @@
+export * from './queries/FindText';
+export * from './utils';
+
--- packages/pdf/tsup.config.ts
+++ packages/pdf/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/core/index.ts', 'src/query/index.ts', 'src/ops/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
